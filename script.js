0198fbeb1c5074630c4b583dd1272d6daa66d11e
dew613 <dewansunnah613@gmail.com>
2022-03-15 23:44:33 -0400

    finished the adding rows function

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ function addR() {
     //alert("Clicked Add Row")
     let grid = document.getElementById("grid");
     let rows = document.getElementsByTagName("tr");
-    console.log(rows.length);
+    // console.log(rows.length);
+    // console.log(grid)
+    // console.log(rows)
+    // if(rows.length > 0)
+    //     console.log(rows[0].childElementCount)
     
     if (rows.length === 0) {
+        // console.log("row == 0 happens")
         let row = document.createElement("tr");
         let col = document.createElement("td");
         col.onclick = function (){
@@ -17,12 +22,28 @@ function addR() {
         grid.appendChild(row);
 
     }
+
+    else{
+        let new_row = document.createElement("tr");
+        for(r = 0; r < rows[0].childElementCount; r++){
+            let new_col_element = document.createElement("td");
+            new_col_element.onclick = function (){
+                this.style.backgroundColor = colorSelected;
+            };
+            // console.log("for loop happens")
+            new_row.appendChild(new_col_element)
+            // console.log(new_row)
+        }
+        grid.appendChild(new_row)
+    }
 }
 //Adds a column
 function addC() {
     //alert("Clicked Add Col")
     let cols = document.getElementsByTagName("td");
     console.log(cols.length); 
+
+    
 }
 
 //Removes a row
